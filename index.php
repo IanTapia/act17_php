2230c02bc87bb538c47dfd29c177d867ed92be89
IanTapia1 <125591984+IanTapia@users.noreply.github.com>
2023-03-29 14:21:31 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+  <head>
+    <title>Datos</title>
+  </head>
+  <body>
+    <?php
+       $nombre = "Adrik Ian Tapia Valero";
+       $calle = "calle pinosuarez";
+       $numero = "1985";
+       $colonia = "limaslomas";
+       $municipio = "Saltillo coahuila";
+       $telefono = "844 321 1098";
+
+       echo "Nombre completo: $nombre  ";
+        ?>
+<br>
+<?php
+       echo "Calle donde vivo: $calle ";
+        ?>
+<br>
+<?php
+       echo "Mi colonia: $colonia ";
+        ?>
+<br>
+<?php
+       echo "Numero de la casa: $numero ";
+        ?>
+<br>
+<?php
+       echo " Municipio: $municipio  ";
+       ?>
+<br>
+<?php
+       echo "Mi telefono es:$telefono";
+    ?>
+  </body>
+</html>
